3048ba7c39a2cdbf3fb80b53dbddf2d767f498a8
DennisHuang <zxc22661560@gmail.com>
2023-12-14 12:09:44 +0800

    Add argpares argument code and Provide export tensorrt accuracy option in code.

--- convert.py
+++ convert.py
@@ -1,20 +1,36 @@
+import argparse
 import os
-from glob import glob
-
 import numpy as np
-import tensorflow as tf
 from tensorflow.python.compiler.tensorrt import trt_convert
 
 from TensorFlow_model.deeplab_v3plus import *
 from TensorFlow_model.segformer import *
-from Utils.dataset import CityScapes
-from Utils.utils import *
 
 
-def convert_tensorrt(model_dir, tensorrt_dir):
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str , required=True , default='', 
+                        help='Tensorflow model location.')
+    parser.add_argument('--model-type', choices=['Deeplabv3+', 'Segformer'] , required=True , 
+                        help='Type of model will convert to Tensorrt. Deeplabv3+ or Segformer')
+    parser.add_argument('--export-dir', default='',
+                        help='Export directory of Tensorrt model.')
+    parser.add_argument('--h5', action='store_true',
+                        help='If you load .h5 model.')
+    parser.add_argument('--fp16', action='store_true',
+                        help='Inference fp16 accuracy default is fp32.')
+
+    return parser.parse_known_args()[0]
+
+def convert_tensorrt(model_dir, tensorrt_dir, fp16=False):
+    precision = trt_convert.TrtPrecisionMode.FP32
+    if fp16:
+        precision = trt_convert.TrtPrecisionMode.FP16
+
     converter = trt_convert.TrtGraphConverterV2(
         input_saved_model_dir=model_dir,
-        precision_mode=trt_convert.TrtPrecisionMode.FP32
+        precision_mode=precision
     )
     converter.convert()
 
@@ -26,42 +42,40 @@ def convert_tensorrt(model_dir, tensorrt_dir):
         converter.build(input_fn=input_fn)
     converter.save(output_saved_model_dir=tensorrt_dir)
 
-# Convert deeplab_v3+ to tensorrt model
-# model = tf.keras.models.load_model('Checkpoints/deeplabv3plus/deeplabv3plus_inception_256x512.h5', compile=False)
-# tf.saved_model.save(model, 'Checkpoints/deeplabv3plus/export/deeplabv3plus_inception_256x512')
-# convert_tensorrt(model_dir='Checkpoints/deeplabv3plus/export/deeplabv3plus_inception_256x512',
-#                  tensorrt_dir="Checkpoints/deeplabv3plus/export/tensorrt/")
-
-# Convert segformer to tensorrt model
-model = tf.keras.models.load_model('Checkpoints/segformer/segformerb2_256x512.h5', compile=False)
-tf.saved_model.save(model, 'Checkpoints/segformer/export/segformerb2_256x512')
-
-convert_tensorrt(model_dir='Checkpoints/segformer/export/segformerb2_256x512',
-                 tensorrt_dir="Checkpoints/segformer/export/tensorrt/")
-
-
-# test tensorrt
-dataset = CityScapes(image_shape=(256, 512, 3), batch_size=16, one_hot=True)
-
-model = tf.keras.models.load_model('Checkpoints/deeplabv3plus/export/tensorrt')
-test_path = sorted(glob(os.path.join(dataset.image_path, "test/*")))
-index = np.random.choice(len(test_path), 5, False).astype(np.uint8)
-
-test_path = np.array(test_path)[index]
-test_img = []
-
-for var in test_path:
-    test_img.append(dataset.read_image(var))
-test_img = np.array(test_img)
-colormap = dataset.color_table[:-1]
-
-for var in test_img:
-    pre = infer(model, var, 'tensorrt')
-    mask = predict2mask(pre, colormap)
-    origin, overlay = get_visualize(var, mask)
-    _, axes = plt.subplots(nrows=1, ncols=3, figsize=(4, 3))
-    axes[0].imshow(origin)
-    axes[1].imshow(overlay)
-    axes[2].imshow(mask)
-    plt.show()
+if __name__ == '__main__':
+    opt = parse_opt()
+    fp16 = False
+    if opt.fp16:
+        fp16 = True
+    if opt.model_type == 'Deeplabv3+':
+        # Convert deeplab_v3+ to tensorrt model
+        SAVEDIR = opt.model
+        EXPORTDIR = opt.export_dir
+        if not opt.export_dir:
+            EXPORTDIR = 'Checkpoints/deeplabv3plus/tensorrt_deeplabv3plus_inception_256x512'
+            if fp16: EXPORTDIR += '_FP16'
+            else: EXPORTDIR += '_FP32'
+            os.makedirs(EXPORTDIR, exist_ok=True)
+        if opt.h5:
+            model = tf.keras.models.load_model(SAVEDIR, compile=False)
+            SAVEDIR = 'Checkpoints/deeplabv3plus/deeplabv3plus_inception_256x512'
+            os.makedirs(SAVEDIR, exist_ok=True)
+            tf.saved_model.save(model, SAVEDIR)
+    elif opt.model_type == 'Segformer':
+        # Convert Segformer to tensorrt model
+        SAVEDIR = opt.model
+        EXPORTDIR = opt.export_dir
+        if not opt.export_dir:
+            EXPORTDIR = 'Checkpoints/segformer/tensorrt_segformerb2_256x512'
+            if fp16: EXPORTDIR += '_FP16'
+            else: EXPORTDIR += '_FP32'
+            os.makedirs(EXPORTDIR, exist_ok=True)
+        if opt.h5:
+            model = tf.keras.models.load_model(SAVEDIR, compile=False)
+            SAVEDIR = 'Checkpoints/segformer/segformerb2_256x512'
+            os.makedirs(SAVEDIR, exist_ok=True)
+            tf.saved_model.save(model, SAVEDIR)
+    
+    convert_tensorrt(model_dir=SAVEDIR, tensorrt_dir=EXPORTDIR, fp16=fp16)
+    print("Finish. Your model location: {}".format(EXPORTDIR))
 
